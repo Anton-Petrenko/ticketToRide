08a5ca496e03350839dd08c3a0e299975cf60261
Anton-Petrenko <antonp.send@gmail.com>
2024-03-29 15:21:20 -0400

    last commit before adjusting entire turn gen

--- models/mcts.py
+++ models/mcts.py
@@ -1,3 +1,13 @@
+"""
+TODO: How do we denote actions in TTR MCTS?
+
+Output Heads:
+1. Action
+2. Desire of color
+3. Desire of destination
+
+"""
+
 class Node:
     """The nodes in the MCTS tree"""
     def __init__(self, prior, root=True) -> None:
@@ -5,18 +15,18 @@ class Node:
         self.prior = prior              # P(s,a)
         self.visitCount = 0             # N(s,a)
         self.aggregateWinningProb = 0   # W(s,a)
-        self.children = [Node]              
+        self.children = list[Node]              
         """The children of the node - of type [ Node, Node, ... ]"""
         self.fromAction = list[list]
-        """Each node that is a child stores the action (from its parent) that led to it - of type (p, Dc, Dd)"""
+        """Each node that is a child stores the action (from its parent) that led to it - of type ???"""
 
-    def getMeanWinningProb(self):
+    def getMeanWinningProb(self) -> float:
         """Return Q(s,a) - the mean winning probaility for a node/state"""
         if self.visitCount == 0:
-            return 0
-        return self.aggregateWinningProb / self.visitCount
+            return float(0)
+        return float(self.aggregateWinningProb / self.visitCount)
 
 class MonteCarloSearch:
     """The Monte Carlo class for the Ticket to Ride Engine"""
-    def __init__(self) -> None:
-        pass
+    def __init__(self, root: Node) -> None:
+        self.root = root
